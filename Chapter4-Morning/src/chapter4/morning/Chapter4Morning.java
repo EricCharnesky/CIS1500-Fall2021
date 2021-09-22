b84872ec26f96b42d2427ec32b5b18c6c945cb3b
Eric Charnesky <eric.charnesky@careevolution.com>
2021-09-22 11:46:56 -0400

    moved for loops down

--- Chapter4-Morning/src/chapter4/morning/Chapter4Morning.java
+++ Chapter4-Morning/src/chapter4/morning/Chapter4Morning.java
@@ -7,57 +7,6 @@ public class Chapter4Morning {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        // todo - move me back down
-        System.out.println("What size square do you want to print?");
-        int squareSize = Integer.parseInt(keyboard.nextLine());
-        for (int row = 1; row <= squareSize; row++) {
-            for (int star = 1; star <= squareSize; star++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-
-        System.out.println("What height rectangle do you want?");
-        int rectangleHeight = Integer.parseInt(keyboard.nextLine());
-
-        System.out.println("What width rectangle do you want?");
-        int rectangleWidth = Integer.parseInt(keyboard.nextLine());
-
-        for (int row = 1; row <= rectangleHeight; row++) {
-            for (int star = 1; star <= rectangleWidth; star++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-
-        System.out.println("What size triangle do you want?");
-        int triangleSize = Integer.parseInt(keyboard.nextLine());
-
-        for (int row = 1; row <= triangleSize; row++) {
-            for (int star = 1; star <= row; star++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-
-        // making sure it's an odd size
-        if (triangleSize % 2 == 0) {
-            triangleSize++;
-        }
-        int spaces = triangleSize / 2; // integer division is good here
-
-        for (int stars = 1; stars <= triangleSize; stars += 2) {
-            for (int space = 1; space <= spaces; space++) {
-                System.out.print(" ");
-            }
-            for (int star = 1; star <= stars; star++) {
-                System.out.print("*");
-            }
-            spaces--;
-            System.out.println("");
-        }
-
         System.out.println("Enter a number: ");
         int usersNumber = Integer.parseInt(keyboard.nextLine());
 
@@ -252,6 +201,57 @@ public class Chapter4Morning {
             for (int minute = 0; minute < 60; minute++) {
                 System.out.println(hour + ":" + minute);
             }
+        }
+        
+        // todo - move me back down
+        System.out.println("What size square do you want to print?");
+        int squareSize = Integer.parseInt(keyboard.nextLine());
+        for (int row = 1; row <= squareSize; row++) {
+            for (int star = 1; star <= squareSize; star++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("What height rectangle do you want?");
+        int rectangleHeight = Integer.parseInt(keyboard.nextLine());
+
+        System.out.println("What width rectangle do you want?");
+        int rectangleWidth = Integer.parseInt(keyboard.nextLine());
+
+        for (int row = 1; row <= rectangleHeight; row++) {
+            for (int star = 1; star <= rectangleWidth; star++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("What size triangle do you want?");
+        int triangleSize = Integer.parseInt(keyboard.nextLine());
+
+        for (int row = 1; row <= triangleSize; row++) {
+            for (int star = 1; star <= row; star++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+
+        // making sure it's an odd size
+        if (triangleSize % 2 == 0) {
+            triangleSize++;
+        }
+        int spaces = triangleSize / 2; // integer division is good here
+
+        for (int stars = 1; stars <= triangleSize; stars += 2) {
+            for (int space = 1; space <= spaces; space++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= stars; star++) {
+                System.out.print("*");
+            }
+            spaces--;
+            System.out.println("");
         }
 
     }
