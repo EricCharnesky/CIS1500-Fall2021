700430cf2193355b0f6c1250b1e8d79a3aa58647
Eric Charnesky <eric.charnesky@careevolution.com>
2021-10-19 19:23:53 -0400

    midterm review

---
+++ MidtermReview/src/midtermreview/Flower.java
@@ -0,0 +1,41 @@
+
+package midtermreview;
+
+public class Flower {
+    private String name;
+    private String color;
+    private String smell;
+    private int numberOfPetals;
+
+    public Flower(String name, String color, String smell, int numberOfPetals) {
+        this.name = name;
+        this.color = color;
+        this.smell = smell;
+        this.numberOfPetals = numberOfPetals;
+    }
+
+    public void pickAPetal(){
+        
+        if (numberOfPetals > 0 ){
+            numberOfPetals--;
+        }
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSmell() {
+        return smell;
+    }
+
+    public int getNumberOfPetals() {
+        return numberOfPetals;
+    }
+    
+    
+}
