85060d88ec87b332993d4114573ba24eff1476e8
Eric Charnesky <eric.charnesky@careevolution.com>
2021-11-02 18:27:54 -0400

    midterm answers

--- MidtermReview/src/midtermreview/MidtermReview.java
+++ MidtermReview/src/midtermreview/MidtermReview.java
@@ -15,9 +15,30 @@ public class MidtermReview {
         return result;
     }
     
+    public static int sumOfEvenNumbersUpToN(int number){
+        int sum = 0;
+        //for ( int value = 2; value <= number; value += 2)
+         for ( int value = 0; value <= number; value ++){
+             if ( value % 2 == 0){
+                 sum += value;
+             }
+         }
+         return sum;
+    }
+    
     public static void main(String[] args) {
-        String className = "";
+        
+        String moreItems = "y";
         Scanner keyboard = new Scanner(System.in);
+        
+        while ( moreItems.equalsIgnoreCase("y")){
+            System.out.println("Enter an item to add to your shopping list");
+            String item = keyboard.nextLine();
+            System.out.println("Do you have more items to add to your list? y/n");
+            moreItems = keyboard.nextLine();
+        }
+        
+        String className = "";
         
         while ( !className.equalsIgnoreCase("ALLDONE")){
             System.out.println("Please enter the name of your class or ALLDONE to stop");
